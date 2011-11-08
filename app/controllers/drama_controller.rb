5c58f8d489194683a48c4e9a1bc5e5da32c38b26
David Thompson <dtt101@gmail.com>
2011-11-08 18:11:00 +0000

    muchos todos

--- app/controllers/drama_controller.rb
+++ app/controllers/drama_controller.rb
@@ -1,6 +1,15 @@
 class DramaController < ApplicationController
   
+  # TODO - auth on scene, line and performance admin
+  # TODO - add in design
+  # TODO - seeds.rb - add lines
+  # TODO - line design
+  # TODO - update README
+  # TODO - Add in ability for users to submit YouTube links with email and twitter
+  # TODO - look at moderation for YouTube videos
+  
   def index
+    
   end
 
 end
