8ff5ee66999af5b9ae46febd3ef1eab1819e4cc1
David Thompson <dtt101@gmail.com>
2011-11-17 18:18:34 +0000

    SImple URL validation
    There will be a human approval method *anyway* but this is worthwhile

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,6 +1,6 @@
 class Performance < ActiveRecord::Base
   belongs_to :line
   
-  validates :video_url, presence: true
+  validates :video_url, presence: true, :format => URI::regexp(%w(http https))
   
 end
