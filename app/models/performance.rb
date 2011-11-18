1f26815d7f1c86f501c18e80dc42b57de3f5c604
David Thompson <dtt101@gmail.com>
2011-11-18 16:43:31 +0000

    Update to Rails 3.1.2

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,5 +1,5 @@
 class Performance < ActiveRecord::Base
-  belongs_to :line, #:counter_cache => true see http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html
+  belongs_to :line #, :counter_cache => true see http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html
   
   validates :video_url, presence: true, :format => URI::regexp(%w(http https))
   
