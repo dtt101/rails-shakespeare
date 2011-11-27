15c18d25c7c6093b20cd3ace4ce76a610af0a301
David Thompson <dtt101@gmail.com>
2011-11-27 23:46:35 +0000

    Replaced fugly JQuery UI with lovely colorbox

--- app/assets/javascripts/drama.js
+++ app/assets/javascripts/drama.js
@@ -1,26 +1,20 @@
 // drama js
 
-$(document).ready(function(){
-  $("a.new-performance-link").click(function(e) {
-    lineid = $(this).data("line")
-    $("#line_id").val(lineid); // pass line id
-    // copy line and character to dialog
-    $("#new-performance .character").html($("#line-character-" + lineid).html());
-    $("#new-performance .line").html($("#line-" + lineid).html());
-    //$('.hello').clone().appendTo('.goodbye');
-    $("#new-performance").dialog( "open" );
-    e.preventDefault();
-  });
- 
-  $("#new-performance").dialog({
-      autoOpen: false,
-      height: "auto",
-      width: 550,
-      modal: true,
-      position: "top",
-      draggable: false,
-      resizable: false,
-      hide: { effect: "blind", options: {}, speed: 1000 },
-      show: { effect: "blind", options: {}, speed: "fast" }
-  });  
+$(document).ready(function() {
+  // set up link for new performance
+  $("a.new-performance-link").colorbox({inline:true, width:"50%"});
+  // add in line on open event
+  $("a.new-performance-link").colorbox({
+  	onOpen:function(){ 
+      lineid = $(this).data("line")
+      $("#line_id").val(lineid); // pass line id
+      // copy line and character to dialog
+      $("#new-performance").show();
+      $("#new-performance .character").html($("#line-character-" + lineid).html());
+      $("#new-performance .line").html($("#line-" + lineid).html());
+  	  },
+  	onClosed:function() {
+      $("#new-performance").hide();  	  
+	  },
+	});
 });
