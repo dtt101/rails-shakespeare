dc5d561fc0257f84f7b3cc7706b0458d9ec4cc5a
David Thompson <dtt101@gmail.com>
2011-12-14 14:32:30 +0000

    Sorted out tabs, added credits

--- app/assets/javascripts/drama.js
+++ app/assets/javascripts/drama.js
@@ -1,20 +1,21 @@
 // drama js
 
 $(document).ready(function() {
-  // set up link for new performance
-  $("a.new-performance-link").colorbox({inline:true, width:"50%"});
-  // add in line on open event
-  $("a.new-performance-link").colorbox({
-  	onOpen:function(){ 
-      lineid = $(this).data("line")
-      $("#line_id").val(lineid); // pass line id
-      // copy line and character to dialog
-      $("#new-performance").show();
-      $("#new-performance .character").html($("#line-character-" + lineid).html());
-      $("#new-performance .line").html($("#line-" + lineid).html());
-  	  },
-  	onClosed:function() {
-      $("#new-performance").hide();  	  
-	  },
-	});
+    // set up link for new performance
+    $("a.new-performance-link").colorbox({inline:true, width:"50%"});
+    
+    // add in line on open event
+    $("a.new-performance-link").colorbox({
+        onOpen:function() { 
+            lineid = $(this).data("line");
+            $("#line_id").val(lineid); // pass line id
+            // copy line and character to dialog, then show
+            $("#new-performance .character").html($("#line-character-" + lineid).html());
+            $("#new-performance .line").html($("#line-" + lineid).html());
+            $("#new-performance").show();
+        },
+      	onClosed:function() {
+            $("#new-performance").hide();  	  
+        },
+    });
 });
