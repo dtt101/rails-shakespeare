9b42e5e4bfa52273ee3d6fcff16ca71132a1e46f
David Thompson <dtt101@gmail.com>
2012-01-23 20:32:34 +0000

    update to 3.2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Shakespeare::Application.config.secret_token = '1485d36b8e9319495a8843ba61aef6829fe22ce7ef8c1b487b996d4a735152e5b76b6bded64b31bb50d8ade4bb3552a8d8fd1b5eef00b9435d56d46618ba2c81'
+Shakespeare::Application.config.secret_token = 'c37401d618f1fdb58b1dbddbf5436f23aeb1637f9c5d1737a7c2c7737230bd1025deb192cee8095cf5783f36c82725d6b79af20bd32870fc073ea76098c37fae'
