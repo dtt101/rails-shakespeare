522048322ef2283499c04e519bb3b35ac18f6af8
David Thompson <dtt101@gmail.com>
2012-02-10 11:19:18 +0000

    simplest of all error handling

--- app/controllers/drama_controller.rb
+++ app/controllers/drama_controller.rb
@@ -16,13 +16,14 @@ class DramaController < ApplicationController
     # ensure approved is false
     @performance.approved = false
     line = Line.find(params[:line_id])
+    
     respond_to do |format|
       if line.performances.push(@performance)
-        format.html { redirect_to :action =>'index', notice: 'success' }
-        format.js { render json: @performance, status: :created, location: @performance }
+        # TODO - no js?
+        format.js
       else
-        format.html { redirect_to :action => 'index', notice: 'failure' }
-        format.js { render json: @performance.errors, status: :unprocessable_entity }
+        # TODO - no js?
+        format.js
       end
     end    
     
