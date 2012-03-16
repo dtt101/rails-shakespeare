83d10c7850dc9633f8e1d9c185d38f50a2bc41d9
David Thompson <dtt101@gmail.com>
2012-03-16 17:15:53 +0000

    admin users can change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,46 @@ class UsersController < ApplicationController
     end
   end
   
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+
   def edit
     @user = User.find(params[:id])
   end
   
   def update
-     
+    logger.info params
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
   def destroy
-    # TODO
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if User.count == 1
+        format.html { redirect_to user_url, notice: 'There is only one user left, you can\'t destroy it, silly' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      else 
+        @user.destroy
+        format.html { redirect_to users_url }
+        format.json { head :ok }
+      end
+    end
+
   end
 end
